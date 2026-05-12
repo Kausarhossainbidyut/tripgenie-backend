792e5fdd9bc0e068d59292b3914feb3f6693c2dd
KH Bidyut <khbidyut31@gmail.com>
2026-05-12 14:46:11 +0600

    update origin url

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:3000','https://tripgenie-backend.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
